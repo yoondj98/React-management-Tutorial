8e0b8b560e445d55cafb0d0bcc5356fa45f9fed4
Xerath <yoondj98@naver.com>
2021-09-14 19:40:18 +0900

    Create Customers.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,57 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div className = "gray-background">
-        <img src = {logo} lat ="logo"/>
-        <h2> Let's develop management system!</h2>
+import Customer from './components/Customer'
+
+const customers = [
+  {
+  "id" :1,
+  "image" : "https://placeimg.com/64/64/1",
+  'name' :'윤동주',
+  'birthday' : "980817",
+  "gender" : "남성",
+  "job" : "대학생"
+  },
+  {
+    "id" :2,
+    "image" : "https://placeimg.com/64/64/2",
+    'name' :'김동주',
+    'birthday' : "980815",
+    "gender" : "여성",
+    "job" : "고등학생"
+    },
+    {
+      "id" :3,
+      "image" : "https://placeimg.com/64/64/3",
+      'name' :'간동주',
+      'birthday' : "980819",
+      "gender" : "중성",
+      "job" : "중학생"
+      }
+    ]
+class App extends Component {
+  render(){
+    return (
+      <div>
+        {customers.map(c=> {
+          return (
+            <Customer
+            key = {c.id}
+            id = {c.id}
+            image = {c.image}
+            name ={c.name}
+            birthday = {c.birthday}
+            gender = {c.gender}
+            job = {c.job}
+            />
+          )
+        })}
+
+
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
